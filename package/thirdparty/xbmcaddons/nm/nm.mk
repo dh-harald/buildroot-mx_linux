18a9acd06253ce16ddf6d2e7fa867c6b59569ac5
dh-harald <github@criminal.hu>
2014-04-25 19:45:32 +0200

    script.linux.nm -> script.mxlinux.wifimanager

--- package/thirdparty/xbmcaddons/nm/nm.mk
+++ package/thirdparty/xbmcaddons/nm/nm.mk
@@ -3,16 +3,16 @@
 ## Xbmc NetworkManager Addon
 #
 ###############################################################################
-NM_VERSION = 2c38cb0a11eb82ab71856a20f2d23ae0bb4398a9
+NM_VERSION = wip
 NM_SOURCE=nm-$(NM_VERSION).tar.gz
-NM_SITE = git://github.com/CoreTech-Development/script.linux.nm.git
+NM_SITE = git://github.com/CoreTech-Development/script.mxlinux.wifimanager.git
 NM_SITE_METHOD = git
 NM_INSTALL_STAGING = NO
 NM_INSTALL_TARGET = YES
 
 define NM_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/share/xbmc/addons/script.linux.nm
-	cp -rf $(@D)/* $(TARGET_DIR)/usr/share/xbmc/addons/script.linux.nm/
+	mkdir -p $(TARGET_DIR)/usr/share/xbmc/addons/script.mxlinux.wifimanager
+	cp -rf $(@D)/* $(TARGET_DIR)/usr/share/xbmc/addons/script.mxlinux.wifimanager/
 endef
 
 $(eval $(call xbmc-addon,package/thirdparty/xbmcaddons,nm))
